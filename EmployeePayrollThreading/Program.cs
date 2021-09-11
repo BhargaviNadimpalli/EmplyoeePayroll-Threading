98a64203dda8f9a0cb47098a921d6903b9c537ef
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-11 10:20:27 -0600

    Welcome Message

---
+++ EmployeePayrollThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee payroll programs using thread");
+        }
+    }
+}
